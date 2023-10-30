968a970a8179e85276b7e9e28e4b07ffe09fdc2b
Massimo Pennazio <maxipenna@libero.it>
2023-10-30 13:03:44 +0100

    Sync as per RTNeural b8dc376

--- model_utils.py
+++ model_utils.py
@@ -27,14 +27,31 @@ def save_model_json(model, layers_to_skip=(keras.layers.InputLayer), skip=0, inp
         if isinstance(layer, keras.layers.Conv1D):
             return 'conv1d'
 
+        if isinstance(layer, keras.layers.PReLU):
+            return 'prelu'
+
+        if isinstance(layer, keras.layers.BatchNormalization):
+            if len(layer.input_shape) == 3:
+                return 'batchnorm'
+            elif len(layer.input_shape) == 4:
+                return 'batchnorm2d'
+            else :
+                raise ValueError("Incorrect input_shape when saving batchnorm layer")
+
+        if isinstance(layer, keras.layers.Conv2D):
+            return 'conv2d'
+
+        if isinstance(layer, keras.layers.Activation):
+            return 'activation'
+
         return 'unknown'
 
     def get_layer_activation(layer):
-        if not hasattr(layer, 'activation'):
-            return ''
-
         if isinstance(layer, keras.layers.TimeDistributed):
             return get_layer_activation(layer.layer)
+
+        if not hasattr(layer, 'activation'):
+            return ''
 
         if layer.activation == keras.activations.tanh:
             return 'tanh'
@@ -50,22 +67,43 @@ def save_model_json(model, layers_to_skip=(keras.layers.InputLayer), skip=0, inp
 
         if layer.activation == keras.activations.elu:
             return 'elu'
-        
+
         return ''
+
 
     def save_layer(layer):
         layer_dict = {
             "type"       : get_layer_type(layer),
             "activation" : get_layer_activation(layer),
             "shape"      : layer.output_shape,
-            "weights"    : layer.get_weights()
         }
 
         if layer_dict["type"] == "conv1d":
             layer_dict["kernel_size"] = layer.kernel_size
             layer_dict["dilation"] = layer.dilation_rate
 
+        if layer_dict["type"] == "conv2d":
+            layer_dict["kernel_size_time"] = layer.kernel_size[0]
+            layer_dict["kernel_size_feature"] = layer.kernel_size[1]
+            layer_dict["dilation"] = layer.dilation_rate[0] # only time axis
+            layer_dict["strides"] = layer.strides[1] # only feature axis
+            layer_dict["num_filters_in"] = layer.input_shape[3]
+            layer_dict["num_features_in"] = layer.input_shape[2]
+            layer_dict["num_filters_out"] = layer.output_shape[3]
+            layer_dict["padding"] = str(layer.padding).lower()
+
+        if layer_dict["type"] == "batchnorm":
+            layer_dict["epsilon"] = layer.epsilon
+
+        if layer_dict["type"] == "batchnorm2d":
+            layer_dict["epsilon"] = layer.epsilon
+            layer_dict["num_filters_in"] = layer.input_shape[3]
+            layer_dict["num_features_in"] = layer.input_shape[2]
+
+        layer_dict["weights"] = layer.get_weights()
+
         return layer_dict
+
 
     model_dict = {}
     model_dict["in_shape"] = model.input_shape
